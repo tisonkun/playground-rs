b0705b65c9e0b54e1ae55910e7fdb3edf42710f2
tison <wander4096@gmail.com>
2022-01-24 13:50:31 +0800

    initialize
    Signed-off-by: tison <wander4096@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+#![feature(generators, generator_trait)]
+#![feature(type_name_of_val)]
+
+use std::any::type_name_of_val;
+use std::ops::{Generator, GeneratorState};
+use std::pin::Pin;
+
+fn main() {
+    let mut gen = fab(5);
+    println!("{:?}", type_name_of_val(&gen));
+    loop {
+        match Pin::new(&mut gen).resume(()) {
+            GeneratorState::Yielded(value) => println!("yield {}", value),
+            GeneratorState::Complete(ret) => {
+                println!("return {}", ret);
+                break;
+            }
+        }
+    }
+}
+
+fn fab(mut n: u64) -> impl Generator<Yield=u64, Return=u64> {
+    move || {
+        let mut last = 0u64;
+        let mut current = 1;
+        yield last;
+        while n > 0 {
+            yield current;
+            let tmp = last;
+            last = current;
+            current = tmp + last;
+            n -= 1;
+        }
+        return last;
+    }
+}
