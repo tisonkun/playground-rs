892b6714da47b2f2d62c26fd43b48e499e794538
tison <wander4096@gmail.com>
2022-01-31 11:34:13 +0800

    recursive
    Signed-off-by: tison <wander4096@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,47 @@ use futures::future::BoxFuture;
 
 #[async_trait]
 trait AsyncTrait {
-    async fn async_function(&self) -> Result<String, ()>;
+    async fn async_function(&mut self) -> Result<String, ()>;
 }
 
 trait ManualAsyncTrait {
-    fn manual_async_function(&self) -> BoxFuture<Result<String, ()>>;
+    fn manual_async_function(&mut self) -> BoxFuture<Result<String, ()>>;
 }
 
-struct A;
+struct A(u8);
 
 #[async_trait]
 impl AsyncTrait for A {
-    async fn async_function(&self) -> Result<String, ()> {
-        Ok("async_function".to_string())
+    async fn async_function(&mut self) -> Result<String, ()> {
+        println!("async_function: {}", self.0);
+        if self.0.saturating_sub(1) > 0 {
+            self.0 = self.0.saturating_sub(1);
+            self.manual_async_function().await
+        } else {
+            Ok("async_function".to_string())
+        }
     }
 }
 
 impl ManualAsyncTrait for A {
-    fn manual_async_function(&self) -> BoxFuture<Result<String, ()>> {
-        let f = async { Ok("manual_async_function".to_string()) };
+    fn manual_async_function(&mut self) -> BoxFuture<Result<String, ()>> {
+        let f = async {
+            println!("manual_async_function: {}", self.0);
+            if self.0.saturating_sub(1) > 0 {
+                self.0 = self.0.saturating_sub(1);
+                self.async_function().await
+            } else {
+                Ok("manual_async_function".to_string())
+            }
+        };
         Box::pin(f)
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), ()> {
-    let a = A;
-    println!("{}", a.async_function().await?);
-    println!("{}", a.manual_async_function().await?);
+    println!("{}", A(10).async_function().await?);
+    println!("-------------------------");
+    println!("{}", A(10).manual_async_function().await?);
     Ok(())
 }
